22751c852549534fcfa74dae7eda409a2f024e33
davidfeng <davidfeng.8844@gmail.com>
2023-10-16 18:34:55 +0800

    Add a new file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Repositories/C_C++/C_Exercises/DF1
+# Build directory: D:/Repositories/C_C++/C_Exercises/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
