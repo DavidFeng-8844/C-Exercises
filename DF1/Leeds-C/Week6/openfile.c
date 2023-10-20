f718e8f8740f3773278b8eb2a2907aa874d22c4a
davids-Ubuntu22 <davidfeng.8844@gmail.com>
2023-10-20 11:59:59 +0800

    sync

--- DF1/Leeds-C/Week6/openfile.c
+++ DF1/Leeds-C/Week6/openfile.c
@@ -1,27 +1,34 @@
 #include <stdio.h> 
 int main (){
     FILE *input = fopen("inputfile.txt","w");
-    fprintf(input,"suka");
+    fprintf(input,"suka\nnot suka");
     fclose(input);
+
     input = fopen("inputfile.txt","r");
     FILE *output = fopen("outputfile.txt","w");
     fclose(output);
+
     output = fopen("outputfile.txt","a");
     fprintf(output, "hello world by david\n");
     fprintf(output, "This is a appended line\n");
     fclose(output);
+
     output = fopen("outputfile.txt", "r");
     char iptstr[100];
     char optstr[100];
+
     while(fgets(optstr, 100, output))
     {
         printf("%s\n", optstr);    
     }
-    fgets(iptstr, 100, input);
-    if(iptstr != NULL){
-        printf("%s\n", iptstr);
-    }else{
-        printf("Input file is empty");
+
+    while(fgets(iptstr, 100, input))
+    {
+        if(iptstr != NULL){
+            printf("%s\n", iptstr);
+        }else{
+            printf("Input file is empty");
+        }    
     }
     fclose(input);
     fclose(output);
