13c1fc73407eb6c4ce8648c3f9b0bbf1f40367a2
davids-Ubuntu22 <davidfeng.8844@gmail.com>
2023-10-20 10:31:11 +0800

    create a line

--- DF1/Leeds-C/Week6/openfile.c
+++ DF1/Leeds-C/Week6/openfile.c
@@ -4,12 +4,20 @@ int main (){
     FILE *output = fopen("outputfile.txt","w");
     fclose(output);
     output = fopen("outputfile.txt","a");
-    fprintf(output, "hello world by david\n");
+    fprintf(output, "hello world by david\t");
     fprintf(output, "This is a appended line\n");
    /* if(input == NULL && output == NULL) {
         printf("error opening file\n");
         return 2;
     }*/
+    fclose(output);
+    output = fopen("outputfile.txt", "r");
+    char iptstr[100];
+    char optstr[100];
+    fgets(iptstr, 100, input);
+    fgets(optstr, 100, output);
+    printf("The input string is: %s\n", iptstr);
+    printf("The output string is: %s\n", optstr);
     fclose(input);
     fclose(output);
 }
