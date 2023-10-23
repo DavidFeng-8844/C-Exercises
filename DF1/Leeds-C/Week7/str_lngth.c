fb00e53478ab665f9db514a8cd285de69fc29901
davidfeng <davidfeng.8844@gmail.com>
2023-10-23 23:24:08 +0800

    Add a strncpy

--- DF1/Leeds-C/Week7/str_lngth.c
+++ DF1/Leeds-C/Week7/str_lngth.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#define STRNG_LEN 0
+#define STR_CP_LEN 0
+#define STR_CMP 1
 int main(){
+    
     char str[100];
     char *p = str;
     int i = 0;
+
+    #if STRNG_LEN
     printf("Enter a string: ");
     gets(str);
     while(str[i] != '\0'){
@@ -16,8 +22,33 @@ int main(){
     printf("\nThe length of the string is %d caclulated by the second way\n", i);
     printf("The String is %s", str);
     printf("\u2764\uFE0F\n");
+
+    #endif
+    #if STR_CP_LEN
     char cp_str[50];
     strncpy(cp_str, str, 50);
     printf("The copied string is: %s", cp_str);
+    #endif
+
+    #if STR_CMP 
+    printf("Enter a apple product that you can afford: ");
+    gets(str);
+    if(strncmp(str, "iPhone", 6) == 0){
+        printf("You are rich\n");
+    }
+    else{
+        printf("You are poor\n");
+    }
+    if(strcmp(str, "iPhone") < 0){
+        printf("The ASCII code of the input is smaller");
+    }
+    else if(strcmp(str, "iPhone") > 0){
+        printf("The ASCII code of the input is larger");
+    }
+    
+    #endif
+
+
+
     return 0;
 }
