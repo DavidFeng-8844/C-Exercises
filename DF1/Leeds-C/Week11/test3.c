7a30e8de4df075b077d604b6d421f43542b9ca29
davidfeng <davidfeng.8844@gmail.com>
2023-11-30 22:30:31 +0800

    Update a dice C code

--- DF1/Leeds-C/Week11/test3.c
+++ DF1/Leeds-C/Week11/test3.c
@@ -1,19 +1,12 @@
 #include <stdio.h> 
 int main()
 {
-int x = 30; // original x
-printf("x in outer block: %d at %p\n", x, &x);
-{
-int x = 77; // new x, hides first x
-printf("x in inner block: %d at %p\n", x, &x);
-}
-printf("x in outer block: %d at %p\n", x, &x);
-while (x++ < 33) // original x
-{
-int x = 100; // new x, hides first x
-x++;
-printf("x in while loop: %d at %p\n", x, &x);
-}
-printf("x in outer block: %d at %p\n", x, &x);
+int num = 44;
+int * const a = &num;
+printf("%i\n",*a);
+printf("%i\n", a);
+*a = 18;
+char * str = "";
+printf("%d", *a);
 return 0;
 }
